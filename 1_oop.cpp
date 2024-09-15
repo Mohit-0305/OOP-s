027d842cc89401a60e6e152d59569a5a1156c566
Mohit-0305 <khandalemohit00@gmail.com>
2024-09-15 19:04:14 +0530

    _

---
+++ 1_oop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 3,b = 8;
+    float c = 3.14,d = 6.7;
+    double e = 4587.45,f = 78754.124;
+    char g = '#',h = '!';
+
+    cout<<"Sum of "<<a<<" & "<<b<<" is "<<a+b<<endl;
+
+    return 0;
+}
