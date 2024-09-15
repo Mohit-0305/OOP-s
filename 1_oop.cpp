75fb5fca673bf99d9b7b80c50b7f498fc74986a2
Mohit-0305 <khandalemohit00@gmail.com>
2024-09-15 19:18:51 +0530

    _

--- 1_oop.cpp
+++ 1_oop.cpp
@@ -5,9 +5,12 @@ int main(){
     int a = 3,b = 8;
     float c = 3.14,d = 6.7;
     double e = 4587.45,f = 78754.124;
-    char g = '#',h = '!';
+    char char1 = '#',char2 = '!';
 
     cout<<"Sum of "<<a<<" & "<<b<<" is "<<a+b<<endl;
+    cout<<"Sum of "<<c<<" & "<<d<<" is "<<c+d<<endl;
+    cout<<"Sum of "<<e<<" & "<<f<<" is "<<e+f<<endl;
+    cout<<"Value of character char1 is '"<<char1<<"' & char2 is '"<<char2<<"'"<<endl;
 
     return 0;
 }
